6dc4a8a1cf433cb38b83cf97b7eadc246ca497a7
wangjian <896656211@qq.com>
2018-07-02 20:48:09 +0800

    导入数据库扩展,并配置相关信息

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -6,7 +7,14 @@ app = Flask(__name__)
 class Config(object):
     '''工程配置信息'''
     DEBUG = True
+    #数据库配置信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:wangjian@127.0.01:3306/info5"
+    SQLAICHEMY_TRACK_MODIFICATIONS = False
+
+
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+
 
 @app.route('/index')
 def index():
