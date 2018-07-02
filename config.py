4346ed475c2e83c82370b0219f18234e1722f7b2
wangjian <896656211@qq.com>
2018-07-02 21:23:06 +0800

    创建config配置文件

---
+++ config.py
@@ -0,0 +1,20 @@
+import redis
+
+
+class Config(object):
+    '''工程配置信息'''
+    DEBUG = True
+    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
+    #数据库配置信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:wangjian@127.0.01:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # flask_session的配置信息
+    SESSION_TYPE = "redis"
+    SESSION_USE_SIGHER = True
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    PERMANENT_SESSION_LIFETIME = 86400
--- manage.py
+++ manage.py
@@ -5,27 +5,14 @@ from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from config import Config
 
 app = Flask(__name__)
 
+# 配置
+app.config.from_object(Config)
 
-class Config(object):
-    '''工程配置信息'''
-    DEBUG = True
-    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
-    #数据库配置信息
-    SQLALCHEMY_DATABASE_URI = "mysql://root:wangjian@127.0.01:3306/information"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # redis配置
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    # flask_session的配置信息
-    SESSION_TYPE = "redis"
-    SESSION_USE_SIGHER = True
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    PERMANENT_SESSION_LIFETIME = 86400
 
 
 app.config.from_object(Config)
