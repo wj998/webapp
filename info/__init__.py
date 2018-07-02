611f451c2a01977b391f7d480030e5f53179459f
wangjian <896656211@qq.com>
2018-07-02 21:46:42 +0800

    manage.py 只做最基本的启动工作，将 app 的创建操作移动到 info 的 __init__.py 文件中

---
+++ info/__init__.py
@@ -0,0 +1,23 @@
+import redis
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
+from config import Config
+
+app = Flask(__name__)
+
+# 配置
+app.config.from_object(Config)
+
+# 配置数据库
+db = SQLAlchemy(app)
+
+# 配置redis
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
+# 开启CSRF保护
+CSRFProtect(app)
+
+# 设置session保存位置
+Session(app)
