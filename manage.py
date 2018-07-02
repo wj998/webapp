a7bca08fe307800be1ed5685b74b19fffa04a62c
wangjian <896656211@qq.com>
2018-07-02 21:13:13 +0800

    利用flask-session扩展,将session数据板寸到redis中

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ import redis
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 class Config(object):
     '''工程配置信息'''
     DEBUG = True
+    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
     #数据库配置信息
     SQLALCHEMY_DATABASE_URI = "mysql://root:wangjian@127.0.01:3306/information"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -17,10 +19,19 @@ class Config(object):
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
+    # flask_session的配置信息
+    SESSION_TYPE = "redis"
+    SESSION_USE_SIGHER = True
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    PERMANENT_SESSION_LIFETIME = 86400
+
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
+Session(app)
+
 
 @app.route('/index')
 def index():
